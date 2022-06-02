d870b84441b98633da84795cc334551c517df762
Verendra <v.karri@zensar.com>
2022-06-02 09:43:12 +0530

    first commit

---
+++ EFDBFirstApproach2/Controllers/CategorisController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EFDBFirstApproach2.Models;
+
+namespace EFDBFirstApproach2.Controllers
+{
+    public class CategorisController : Controller
+    {
+        // GET: Categoris
+        public ActionResult Index()
+        {
+            March2022Entities db = new March2022Entities();
+            var categoris = db.tblCategories.ToList();  
+            return View(categoris);
+        }
+
+
+
+
+
+    }
+}
---
+++ EFDBFirstApproach2/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using EFDBFirstApproach2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EFDBFirstApproach2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        March2022Entities db;
+        public ProductsController()
+        {
+            db = new March2022Entities();   
+        }
+        
+        public ActionResult Index()
+        {
+            var allproducts = db.tblProducts.ToList();
+            //var allproducts = db.tblProducts.Where(p => p.catId > 1 && p.price >5000);
+            return View(allproducts);
+        }
+
+        public ActionResult Details(int ?id )
+        {
+            var product= db.tblProducts.Where(p => p.productId == id).FirstOrDefault(); 
+
+            return View(product);
+        }
+
+
+    }
+}
